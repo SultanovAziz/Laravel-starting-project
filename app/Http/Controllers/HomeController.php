a0dee5dd5ca03b9102d9246f6cf1550dd7b49b17
Admin <aziz89131983189@gmail.com>
2022-01-31 23:43:44 +0700

    laravel .env file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,11 @@ class HomeController extends Controller
 {
     public function index()
     {
+//        dump($_ENV["MY_SETTING"]);
+//        dump(config('database.connections.mysql.username'));
+//
+//        dump($_ENV);
+
         return view('home',['res' => 5,'name' => 'Aziz']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/','HomeController@index');
 Route::get('page/{slug}','PageController@show');
 Route::get('test','Test\TestController@index');
-Route::resource('posts','PostController',['parameters' => [
+Route::resource('posts','PostsController',['parameters' => [
     'posts' => 'slug',
 ]]);
 
